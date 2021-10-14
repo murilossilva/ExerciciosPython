9fcf3118546c27282fa1c88a7b14f69658ac2c00
murilossilva <murilossilva@usp.br>
2021-10-14 17:10:13 -0300

    remove docstring exec2

--- Aula - POO/exec2.py
+++ Aula - POO/exec2.py
@@ -1,11 +1,3 @@
-'''
-Classe Quadrado: Crie uma classe que modele um quadrado:
-
-Atributos: Tamanho do lado
-Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
-
-'''
-
 class Square:
     def __init__(self, lado):
         self.lado = lado
