43e487cbaf7262421740614504765c91e12bf5ed
murilossilva <murilossilva@usp.br>
2021-10-16 17:24:26 -0300

    atualiza exec1 tratamento de erros

--- Aula - Testes e Exceções/exec1.py
+++ Aula - Testes e Exceções/exec1.py
@@ -1,7 +1,18 @@
 def divisao(a , b):
     try:
-        return a / b
+        print(a / b)
     except TypeError:
         print(f'Os valores devem ser do tipo int ou float.')
     except ZeroDivisionError:
         print(f'O divisor deve ser diferente de zero!')
+
+def main():
+    try:
+        a = float(input('Insira um valor para o dividendo: '))
+        b = float(input('Insira um valor para o divisor: '))
+        divisao(a, b)
+    except ValueError:
+        print(f'Os valores devem ser do tipo int ou float.')
+
+if __name__ == '__main__':
+    main()
