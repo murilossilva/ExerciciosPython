708ddb4dba316d410d352035dd5b97754dd88ea4
Murilo Souza <murilossilva@usp.br>
2022-01-28 13:09:46 -0300

    update exec validate_cnpj.py

--- validate_cnpj.py
+++ validate_cnpj.py
@@ -8,12 +8,23 @@ def remove_caracteres_especiais(cnpj):
 
 
 def contadores(cnpj_copia):
+    
+    #  cria o contador para verificar o primeiro dígito
     if len(cnpj_copia) == 12:
         contador_1 = count(start = 5, step = -1)
+
+    #  cria o contador para verificar o segundo dígito
     else:
         contador_1 = count(start = 6, step = -1)
     contador_2 = count(start = 9, step = -1)
+    
     return multiplica_algarismos(cnpj_copia, contador_1, contador_2)
+
+
+#  função que irá multiplicar os índices
+def multiplicacao(lista_com_resultados, algarismo, contador):
+    multiplica = int(algarismo) * contador
+    return lista_com_resultados.append(multiplica)
 
 
 def multiplica_algarismos(cnpj_copia, contador_1, contador_2):
@@ -22,20 +33,19 @@ def multiplica_algarismos(cnpj_copia, contador_1, contador_2):
     
     for algarismo_cnpj, contador in zip(cnpj_copia, contador_1):
         if contador >= 2:
-            multiplicacao = int(algarismo_cnpj) * contador
-            lista_com_resultados.append(multiplicacao)
+            multiplicacao(lista_com_resultados, algarismo_cnpj, contador)
             
+    #  laço for para o primeiro dígito         
     if tamanho_cnpj == 12:        
         for algarismo_cnpj, contador in zip(cnpj_copia[4:], contador_2):
             if contador >= 2:
-                multiplicacao = int(algarismo_cnpj) * contador
-                lista_com_resultados.append(multiplicacao)
+                multiplicacao(lista_com_resultados, algarismo_cnpj, contador)
                 
+    #  laço for para o segundo dígito            
     else:
         for algarismo_cnpj, contador in zip(cnpj_copia[5:], contador_2):
             if contador >= 2:
-                multiplicacao = int(algarismo_cnpj) * contador
-                lista_com_resultados.append(multiplicacao)
+                multiplicacao(lista_com_resultados, algarismo_cnpj, contador)
                 
     return formula(sum(lista_com_resultados), cnpj_copia)
 
@@ -48,23 +58,21 @@ def formula(lista_com_resultados, cnpj_copia):
     else:
         pass
     
-    cnpj_copia = cnpj_copia + str(digito)
-    
+    cnpj_copia += str(digito)
+
+    #  realiza a chamada recursiva para o segundo dígito
     if len(cnpj_copia) == 13:
         return contadores(cnpj_copia)
+
+    #  encerra a cascata de funções e retorna o cnpj a ser comparado com o colocado no input
     else:
         return cnpj_copia
 
 
 if __name__ == '__main__':
-    cnpj = input('Digite um cnpj para saber se ele é válido ou não: ')
-    cnpj_copia = remove_caracteres_especiais(cnpj)
-    cnpj = re.sub(r'[^0-9]', '', cnpj)
-    print('É válido' if cnpj == cnpj_copia else 'Não é válido')
-    
-    
-    
-    
-    
+    while True:
+        cnpj = input('Digite um cnpj para saber se ele é válido ou não: ')
+        cnpj_copia = remove_caracteres_especiais(cnpj)
+        cnpj = re.sub(r'[^0-9]', '', cnpj)
+        print('É válido' if cnpj == cnpj_copia else 'Não é válido')
 
-    
